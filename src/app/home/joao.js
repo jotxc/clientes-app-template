4be847c984862a5bd00be2f5a963f395456e41bc
Joao <joaoc3175@gmail.com>
2025-03-22 15:57:18 -0300

    Eu JoaoC acredito na virada

---
+++ src/app/home/joao.js
@@ -0,0 +1,13 @@
+class Joao {
+    constructor() {
+      this.mensagem = "Eu Joao acredito na virada";
+    }
+  
+    mostrarMensagem() {
+      console.log(this.mensagem);
+    }
+  }
+  
+  let joao = new Joao();
+  joao.mostrarMensagem();
+  
